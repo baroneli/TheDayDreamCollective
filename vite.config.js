eec9671ce549c07400f721f4aa4ed57ec93ac81f
Baroneli <baroneli412@gmail.com>
2025-10-23 17:53:37 -0400

    chore: add vite multi-page inputs and fix page scripts

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index:    resolve(__dirname, 'index.html'),
+        about:    resolve(__dirname, 'about.html'),
+        classes:  resolve(__dirname, 'classes.html'),
+        schedule: resolve(__dirname, 'schedule.html'),
+        contact:  resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+});
